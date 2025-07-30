59886d6b6308caa9aaa3a7720c78113f46083318
yurii.krymskyi <yurii.krymskyi@intenselab.com>
2025-07-30 15:39:09 +0200

    show projects from db

--- src/helpers/fetchProjects.ts
+++ src/helpers/fetchProjects.ts
@@ -1,23 +1,16 @@
-import fs from 'fs'
-import { DESKTOP_PATH } from '../config/static.ts'
-import { Project } from '@prisma/client'
-import path from 'path'
+import { Project } from "@prisma/client"
+import { prisma } from "../db.ts"
 
 export const fetchProjects = async (setProjects: (v: Project[]) => void) => {
-  const files = fs.readdirSync(DESKTOP_PATH)
-  const allFiles: Project[] = files.map((file, idx) => {
-    const fullPath = path.join(DESKTOP_PATH, file)
-    const stat = fs.statSync(fullPath)
-
-    return {
-      id: `local-${idx}`,
-      name: file,
-      field0: stat.isDirectory() ? 'folder' : 'file',
-      field1: `${stat.size} bytes`,
-      field2: stat.birthtime.toISOString(),
-      createdAt: stat.birthtime
-    }
-  })
-
-  setProjects(allFiles)
+  try {
+    const projects = await prisma.project.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      }
+    })
+    setProjects(projects)
+  } catch (error) {
+    console.error("Error fetching projects from DB:", error)
+    setProjects([])
+  }
 }
